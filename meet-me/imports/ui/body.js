b93f6aef78398c00a9d855721b90c50776225ed0
roos <roos.uwaerts@ae.be>
2016-04-28 15:00:19 +0200

    Added zalen to db

--- meet-me/imports/ui/body.js
+++ meet-me/imports/ui/body.js
@@ -3,4 +3,10 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
  
 import './body.html';
+import { Rooms } from '../api/rooms.js';
  
+Template.body.helpers({
+  rooms() {
+    return Rooms.find({});
+  },
+});
