43874b419b9707e916d4b7d9e0f2b600e360ac1a
AylaCremmery <ayla.cremmery@gmail.com>
2016-04-28 14:29:39 +0200

    Intro GoogleAPI

--- meet-me/client/main.js
+++ meet-me/client/main.js
@@ -1,22 +1,12 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { Meteor } from 'meteor/meteor';
 
+import '../imports/api/google.js';
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
+Template.getRoute.events({
   'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+    Meteor.call('getroute');
   },
 });
