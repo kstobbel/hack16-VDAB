18f8c0c0d5b446606e7a03ad29097ead8aa28da7
roos <roos.uwaerts@ae.be>
2016-04-28 22:02:26 +0200

    Links

--- meet-me/client/rooms.js
+++ meet-me/client/rooms.js
@@ -6,8 +6,6 @@ import { Mongo } from 'meteor/mongo';
  
 export const Rooms = new Mongo.Collection('rooms');
 
-console.log(Rooms.find({}));
-
 Template.rooms.helpers({
   rooms() {
     return Rooms.find({});
