d97b243188a0138707918102ea2668f0f6b07b0b
AylaCremmery <ayla.cremmery@gmail.com>
2016-04-29 09:06:08 +0200

    Added selected dateinfo to Session

--- meet-me/client/time.js
+++ meet-me/client/time.js
@@ -1,1 +1,12 @@
 import './time.html'
+
+Template.time.events({
+  'change input': function (event) {
+  var currentTarget = event.currentTarget;
+  var newValue = currentTarget.value;
+    if(!Session.equals(currentTarget.name, newValue)){
+      Session.set(currentTarget.name, newValue);
+    }
+    console.log('Input ' + Session.get(currentTarget.name));
+  }
+});
